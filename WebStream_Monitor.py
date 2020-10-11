8896e9106efe43cbf9df2ea038fa79f2af2795ea
james-allgire <55506254+james-allgire@users.noreply.github.com>
2020-10-11 11:58:06 -0400

    Create WebStream_Monitor.py

---
+++ WebStream_Monitor.py
@@ -0,0 +1,33 @@
+import urllib.parse
+import urllib.request
+import json
+
+from fake_useragent import UserAgent
+import requests
+
+
+ua = UserAgent()
+
+url = 'https://api.sermonaudio.com/v1/node/webcasts_in_progress'
+api_key = 'xxxx'
+
+hdr = { 'X-Api-Key' : api_key,'User-Agent':str(ua.chrome)}
+req = urllib.request.Request(url, headers=hdr)
+#with urllib.request.urlopen(req) as response:
+#   the_page = response.read()
+
+with urllib.request.urlopen(req) as response:
+    if response.getcode() == 200:
+        source = response.read()
+        the_page = json.loads(source)
+        print(json.dumps(the_page, indent = 4, sort_keys=True))
+    else:
+        print('An error occurred while attempting to retrieve data from the API.')
+
+
+
+
+
+
+
+
