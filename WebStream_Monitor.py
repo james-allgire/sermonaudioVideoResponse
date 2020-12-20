d5242772cc280a929e4e570c66d70c4ac9384341
james-allgire <55506254+james-allgire@users.noreply.github.com>
2020-12-20 12:00:55 -0500

    Logic to determine is stream is up or down

--- WebStream_Monitor.py
+++ WebStream_Monitor.py
@@ -5,11 +5,11 @@ import json
 from fake_useragent import UserAgent
 import requests
 
-
+#Stop sermon monitoring/alerting at 11:40
 ua = UserAgent()
 
 url = 'https://api.sermonaudio.com/v1/node/webcasts_in_progress'
-api_key = 'xxxxxxxxxxxx'
+api_key = '0F7F0BC2-0B9F-47C8-B7AA-F2FEF0AEF81D'
 
 hdr = {'X-Api-Key' : api_key,'User-Agent':str(ua.chrome)}
 req = urllib.request.Request(url, headers=hdr)
@@ -18,14 +18,15 @@ with urllib.request.urlopen(req) as response:
     if response.getcode() == 200:
         source = response.read()
         the_page = json.loads(source)
-        print(json.dumps(the_page, indent = 4, sort_keys=True))
+        json_results = json.dumps(the_page)
+        try:
+            video_results = the_page['results'][0]['hasVideo']
+            if video_results == True:
+                print("Video Stream is UP") 
+        except (IndexError):
+            video_results = print("Video Stream is OFFLINE!")
+        #print(json.dumps(the_page, indent = 4, sort_keys=True)) #Uncommenting will print out the full json payload
+        #print(video_results) Print single result if video is online
+        
     else:
         print('An error occurred while attempting to retrieve data from the API.')
-
-
-
-
-
-
-
-
